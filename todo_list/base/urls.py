893eb46dde4e3be94e79be5273cffc7d98bbf07b
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-03-29 22:29:39 +0200

    Created basic Class View functionality

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
+from .views import TaskList, TaskDetail, TaskUpdate, TaskCreate, TaskDelete
 
 
 urlpatterns = [
-    path("", views.task_list, name="tasks")
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create_task/', TaskCreate.as_view(), name='create'),
+    path('update_task/<int:pk>/', TaskUpdate.as_view(), name='update'),
+    path('delete_task/<int:pk>/', TaskDelete.as_view(), name='delete')
 ]
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 
-def task_list(request):
-    return HttpResponse("Home Page")
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
