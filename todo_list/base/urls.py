8b2f12a5459d80b11ca544e175f451cc183dd3bc
Łukasz Jabłoński <lukasz.j.pl@interia.pl>
2021-03-31 22:21:52 +0200

    Created user authenticated login, register method

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskUpdate, TaskCreate, TaskDelete
+from .views import TaskList, TaskDetail, TaskUpdate, TaskCreate, TaskDelete, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
@@ -7,5 +8,8 @@ urlpatterns = [
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('create_task/', TaskCreate.as_view(), name='create'),
     path('update_task/<int:pk>/', TaskUpdate.as_view(), name='update'),
-    path('delete_task/<int:pk>/', TaskDelete.as_view(), name='delete')
+    path('delete_task/<int:pk>/', TaskDelete.as_view(), name='delete'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/',RegisterPage.as_view(), name='register' )
 ]
